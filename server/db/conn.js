f61883fcf399b40a91ac07f8e78ea77cf1b1f654
tmc42 <tmclean1212@gmail.com>
2023-01-06 11:49:36 -0600

    Added database connection, tested successfully

---
+++ server/db/conn.js
@@ -0,0 +1,27 @@
+// Database connection
+
+const { MongoClient } = require("mongodb")
+const Db = process.env.ATLAS_URI;
+const client = new MongoClient(Db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+var _db;
+
+module.exports = {
+    connectToServer: function (callback) {
+        client.connect(function (err, db) {
+            if (db)
+            {
+                _db = db.db("employeedb");
+                console.log("Successfully connected to MongoDB.");
+            }
+            return callback(err);
+        });
+    },
+
+    getDb: function () {
+        return _db;
+    },
+};
