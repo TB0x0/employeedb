f1706016481d07f68909c212a5ea1468e48f9d62
tmc42 <tmclean1212@gmail.com>
2023-01-23 18:32:27 -0600

    began mongoose implementation

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,36 +1,17 @@
-const express = require(express);
+const mongoose = require('mongoose');
 
-// Create an express router instance for the users collection
-const userRoutes = express.Router();
+// Define the database schema for the users collection
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, 'Username is required']
+    },
+    created: {
+        type: Date,
+        required: [true, 'Date is required']
+    }
+})
 
-// Connect to the database
-const dbo = require("../db/conn");
+module.exports = userSchema
 
-// Convert the id from string to ObjectId for the _id.
-const ObjectId = require("mongodb").ObjectId;
-
-// Get a user by id
-recordRoutes.route("/user/:id").get(function (req, res) {
-    let db_connect = dbo.getDb();
-    let myquery = { _id: ObjectId(req.params.id) };
-    db_connect
-      .collection("users")
-      .findOne(myquery, function (err, result) {
-        if (err) throw err;
-        res.json(result);
-      });
-   });
-
-   // Create a new user
-recordRoutes.route("/user/add").post(function (req, response) {
-    let db_connect = dbo.getDb();
-    let myobj = {
-      name: req.body.name,
-      position: req.body.position,
-      level: req.body.level,
-    };
-    db_connect.collection("records").insertOne(myobj, function (err, res) {
-      if (err) throw err;
-      response.json(res);
-    });
-   });
+const connectionString = ATLAS_URI;
