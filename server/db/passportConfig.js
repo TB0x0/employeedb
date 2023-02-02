d2e0f47ae7a1564c764c3920d4a8d5bfd4d08ae9
tmc42 <tmclean1212@gmail.com>
2023-02-02 13:04:46 -0600

    Began implementing user authentication server side

---
+++ server/db/passportConfig.js
@@ -0,0 +1,61 @@
+// Passport authentication config
+
+const localStrategy = require("passport-local").Strategy;
+const user = require("../models/users");
+
+module.exports = (passport) => {
+    // User signup function
+    passport.use(
+        "local-signup",
+        new localStrategy(
+            {
+                usernameField: "email",
+                passwordField: "password",
+            },
+            async (email, password, done) => {
+                try {
+                    // Check if user already exists and if not make a new one
+                    const userExists = await user.userTable.findOne({ "email": email });
+                    if (userExists) {
+                        return done(null, false);
+                    }
+                    const newUser = await user.create({ email, password });
+                    return done(null, newUser);
+                } catch (error) {
+                    done(error);
+                }
+            }
+        )
+    );
+    
+    // User login function
+    passport.use(
+        "local-login",
+        new localStrategy(
+            {
+                usernameField: "email",
+                passwordField: "password",
+            },
+            async (email, password, done) => {
+                try {
+                    const userExists = await user.userTable.findOne({ "email": email });
+                    if (!userExists) {
+                        return done(null, false);
+                    }
+
+                    // Check if password matches the correct one
+                    const isCorrect = await user.userTable.matchPassword(password);
+                    if (!isCorrect) {
+                        return done(null, false);
+                    }
+
+                    return done(null, userExists);
+                } catch (error) {
+                    console.log(error);
+                    return done(error, false);
+                }
+            }
+
+        )
+    );
+}
--- server/models/users.js
+++ server/models/users.js
@@ -1,13 +1,35 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.schema;
 
-const userTableSchema = mongoose.Schema({
-    user_id: { type: String, require: true },
-    username: { type: String, require: true },
+const userSchema = mongoose.Schema({
+    email: { type: String, require: true },
     password: { type: String, require: true }
 })
 
-const userTable = mongoose.model("userTable", userTableSchema);
+userSchema.pre('save', async function(next) {
+    try {
+        const user = this;
+        if (!user.isModified('password')) next();
+
+        const salt = await bcrypt.genSalt();
+        const hash = await bcrypt.hash(this.password, salt);
+
+        this.password = hash;
+        next();
+    } catch (error) {
+        return next(error)
+    }
+})
+
+userSchema.methods.matchPassword = async function (password) {
+    try {
+        return await bcrypt.compare(password, this.password);
+    } catch (error) {
+        throw new Error(error);
+    }
+};
+
+const userTable = mongoose.model("users", userSchema);
 
 module.exports = {
     userTable
